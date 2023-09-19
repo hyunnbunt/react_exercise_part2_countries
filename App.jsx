b26d9956947571dbb2d173787bdd24c174eeabf7
unknown <rha542@gmail.com>
2023-09-19 11:36:05 +0900

    2.9

--- App.jsx
+++ App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [keyword, setKeyword] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -39,7 +43,14 @@ const App = () => {
     console.log(newNumber)
   }
 
+  const handleKeywordChange = (event) => {
+    setKeyword(event.target.value)
+  }
+
   const Display = ({person}) => {
+    if (!person.name.toLowerCase().includes(keyword.toLowerCase())) {
+      return
+    }
     return (
       <div>
         {person.name} {person.number}
@@ -51,6 +62,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={keyword} onChange={handleKeywordChange} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />

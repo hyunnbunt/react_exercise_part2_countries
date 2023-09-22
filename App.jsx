58dba9d3fb530d71d8f624c7be490a4bc786fb96
unknown <rha542@gmail.com>
2023-09-22 16:11:59 +0900

    2.18 dataForCountries

--- App.jsx
+++ App.jsx
@@ -1,170 +1,125 @@
-import { useState, useEffect } from 'react'
-import phonebookService from './services/phonebook'
-import PersonForm from './components/Forms'
-import Filter from './components/Filter'
-import './index.css'
+
+import React from "react"
+import {useState, useEffect} from "react"
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [keyword, setKeyword] = useState('')
-  const [message, setMessage] = useState('')
-  let updatingPersonId = -1
+  const [searchCountry, setSearchCountry] = useState('')
+  const [allCountries, setAllCountries] = useState([])
 
   useEffect(() => {
-    phonebookService
-    .getAll()
-    .then(initialPhonebook => {
-      setPersons(initialPhonebook)
-    })
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(res => {
+        setAllCountries(res.data)
+      })
   }, [])
 
-  const Notification = ({message}) => {
-    if (message === null) {
-      return null
-    }
-    return (
-      <div className='message'>
-        {message}
-      </div>
-    )
+  const handleSearchCountryChange = (event) => {
+    setSearchCountry(event.target.value)
   }
 
-  const Persons = () => {
+  const showLanguages = (targetCountry) => {
+    if (targetCountry.hasOwnProperty('languages')) {
+      return (
+        <div>
+          <h2>languages:</h2>
+          <ul>
+            {Object.values(targetCountry.languages).map(language => 
+              <li key={language}>{language}</li>)}
+          </ul>
+        </div>
+      )
+    }
+  }
+
+  const showName = (targetCountry) => {
+    if (targetCountry.hasOwnProperty('name')) {
+      return (
+        <h1>{targetCountry.name.common}</h1>
+      )
+    }
+  }
+
+  
+  const showCapital = (targetCountry) => {
+    if (targetCountry.hasOwnProperty('capital')) {
+      return (
+        <>capital {targetCountry.capital}</>
+      )
+    }
+  }
+
+  const showArea = (targetCountry) => {
+    if (targetCountry.hasOwnProperty('area')) {
+      return (
+        <>area {targetCountry.area}</>
+      )
+    }
+  }
+
+  const showFlag = (targetCountry) => {
+    if (targetCountry.hasOwnProperty('flags')) {
+      return (
+        <img src={targetCountry.flags.png}/>      
+      )
+    }
+  }
+
+  const Result = () => {
+    const filtered = allCountries.filter(country => 
+      country.name.common.toLowerCase()
+      .includes(searchCountry.toLowerCase()))
+
+    if (filtered.length > 10) {
+      return (
+        <div>
+          Too many matches, specify another filter
+        </div>
+      )
+    }
+
+    if (filtered.length === 1) {
+      const targetCountry = filtered[0]
+      console.log(targetCountry)
+
+      return (
+        <div>
+          <div>
+            {showName(targetCountry)}
+          </div>
+          <div>
+            {showCapital(targetCountry)}
+          </div>
+          <div>
+            {showArea(targetCountry)}
+          </div>
+          <div>
+            {showLanguages(targetCountry)}
+          </div>
+          <div>
+            {showFlag(targetCountry)}
+          </div>
+        </div>
+        
+      )
+    }
+
     return (
       <div>
-        {persons.map((person) => {
-          if (person.name.toLowerCase().includes(keyword.toLowerCase())) {
-            return  <Person key={person.name} person={person} />
-          }
-        })}
+        {filtered.map(country =>
+          <ul key={country.name.common}>{country.name.common}</ul>
+        )}
       </div>
     )
-  }
-
-  const Person = ({person}) => {
-    return (
-      <div>{person.name} {person.number} <DeleteButton person={person}/></div>
-    )
-  }
-
-  const DeleteButton = ({person}) => {
-    return (
-      <button onClick={() => deleteName(person)} type="submit">delete</button>
-    )
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleKeywordChange = (event) => {
-    setKeyword(event.target.value)
-  }
-  
-  const addName = (event) => {
-    event.preventDefault()
-    if (isPersonExist()) {
-      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-        updateNumber()
-      } 
-      return
-    }
-
-    const personObject = {
-      name: newName,
-      number: newNumber
-    } 
-
-    phonebookService
-      .create(personObject)
-      .then(createdPerson => {
-        console.log(createdPerson)
-        setPersons(persons.concat(createdPerson))
-        setNewName('')
-        setNewNumber('')
-        setMessage(
-          `${createdPerson.name} is added to phonebook.`
-        )
-        setTimeout(() => {
-          setMessage(null)
-        }, 5000)
-      })
-  }
-
-  const updateNumber = () => {
-    const updatingPersonObject = {
-      name: newName,
-      number: newNumber
-    } 
-    phonebookService
-      .update(updatingPersonId, updatingPersonObject)
-      .then(updatedPerson => {
-        setPersons(persons.map(person => {
-          if (person.id === updatedPerson.id) {
-            return updatedPerson
-          }
-          return person
-        }))
-      })
-      .catch(error => {
-        setMessage(`Information of ${newName} is already removed from server.`)
-      })
-  }
-  
-  const deleteName = (props) => {
-    const confirm = window.confirm(`Delete ${props.name}?`)
-    if (!confirm) {
-      return
-    } 
-    const deletingId = props.id
-    phonebookService
-    .remove(deletingId)
-    .then(
-      setPersons(persons.filter((person) => {
-        if (person.id !== deletingId) {
-          return person
-        }
-      }))
-    )
-  }
-
-  const isPersonExist = () => {
-    let isExist = false
-    persons.forEach((person) => {
-        if (person.name === newName) {
-          updatingPersonId = person.id
-          isExist = true
-          return
-        } 
-    })
-    return isExist
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Notification message={message}/>
-      <Filter 
-        keyword={keyword} 
-        handleKeywordChange={handleKeywordChange} />
-
-      <h3>add a new</h3>
-      <PersonForm 
-        newName={newName} 
-        handleNameChange={handleNameChange} 
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-        addName={addName}
-      />
-
-      <h3>Numbers</h3>
-      <Persons />
+      <form>
+        find countries <input onChange={handleSearchCountryChange} />
+      </form>
+      <Result />
     </div>
   )
 }
